a9725a8175b202ee2d5d43a7e872812f9b508236
Saideep Ullal <saideepullal.ullal@gmail.com>
2020-08-16 14:32:47 +0530

    Added cloud config server
    Integration pending

---
+++ service-one/src/main/java/com/microservicestest/serviceone/controller/ServiceController.java
@@ -0,0 +1,33 @@
+package com.microservicestest.serviceone.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ServiceController {
+	
+	@Autowired
+	Environment environment;
+	
+	@Value("test.property")
+	String cloudconfigProperty;
+	
+	@GetMapping("/getproperty")
+	public ResponseEntity<Map<String,Object>> getCloudConfigProperty() {
+		Map<String,Object> body = new HashMap<String,Object>();
+		body.put("hardcodedProperty", "This is hardcoded");
+		String sampleProperty = environment.getProperty("sample.property");
+		body.put("propertyFromAppProperties", sampleProperty);
+
+		return new ResponseEntity<Map<String,Object>>(body, HttpStatus.OK);
+	}
+
+}
